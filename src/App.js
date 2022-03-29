dc13cbfffc504082119c846f8b0a6f5593bed94c
sebastian.dev <sebastian.villalob@gmail.com>
2022-03-29 16:46:04 -0300

    Some changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Header />
