72cbac96dc17f16dd01ea85c2bb30aa7e8820c58
sebastian.dev <sebastian.villalob@gmail.com>
2022-04-06 16:46:11 -0300

    Añadiendo código

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Detail from './components/Detail';
+import Login from './components/Login';
 import { BrowserRouter, Link, Routes, Route, } from 'react-router-dom';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={ <Home /> } />
           <Route path='/detail' element={ <Detail /> } />
+          <Route path='/login' element={ <Login /> } />
         </Routes>
       </BrowserRouter>
     </div>
