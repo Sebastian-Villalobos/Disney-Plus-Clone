214bec20173d2201e306b2cde50aea364e107ca2
sebastian.dev <sebastian.villalob@gmail.com>
2022-04-07 20:04:14 -0300

    Añadiendo código

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,11 @@ const Login = () => {
     <Container>
         <CTA>
             <CTALogoOne src='/images/cta-logo-one.svg'/>
-            <SignUp>GET ALL THERE</SignUp>
+            <SignUp>CONTRATA OFERTA COMBO +</SignUp>
+            <Description>
+                Contrata el plan mensual de Disney+ y disfrutá los últimos estrenos del cine, originales y clásicos inolvidables.
+            </Description>
+            <CTALogoTwo src='/images/cta-logo-two.png'/>
         </CTA>
     </Container>
   )
@@ -18,7 +22,7 @@ const Container = styled.div`
     position: relative;
     height: calc(100vh - 70px);
     display: flex;
-    align-items: center;
+    align-items: top;
     justify-content: center;
 
     &:before {
@@ -28,6 +32,7 @@ const Container = styled.div`
         bottom: 0;
         left: 0;
         right: 0;
+        opacity: 0.7;
         background-image: url('/images/login-background.jpg');
         background-position: top;
         background-size: cover;
@@ -39,14 +44,18 @@ const Container = styled.div`
 const CTA = styled.div`
     max-width: 650px;
     padding: 80px 40px;
-    width: 80%;
+    width: 90%;
     display: flex;
     flex-direction: column;
-    
+    align-items: center;
 `
 
 const CTALogoOne = styled.img`
     
+`
+
+const CTALogoTwo = styled.img`
+    width: 90%;
 `
 
 const SignUp = styled.a`
@@ -57,4 +66,22 @@ const SignUp = styled.a`
     color: #f9f9f9;
     border-radius: 4px;
     text-align: center;
+    font-size: 18px;
+    cursor: pointer;
+    transition: all 250ms;
+    letter-spacing: 1.5px;
+    margin-top: 8px;
+    margin-bottom: 12px;
+
+    &:hover {
+        background: #0483ee;
+    }
+`
+
+const Description = styled.p`
+    font-size: 11px;
+    letter-spacing: 1.5px;
+    text-align: center;
+    line-height: 1.5;
+
 `
